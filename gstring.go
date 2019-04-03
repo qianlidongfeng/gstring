9c635f54ef0c30aa7154e7e78d18a83b1aaa9c7f
qianlidongfeng <84042019@qq.com>
2019-04-04 01:25:50 +0800

    first commit

---
+++ gstring.go
@@ -0,0 +1,52 @@
+package gstring
+
+import (
+	"bytes"
+	"golang.org/x/text/transform"
+	"io/ioutil"
+	"golang.org/x/text/encoding/simplifiedchinese"
+)
+
+func GbkToUtf8(s []byte) ([]byte, error) {
+	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewDecoder())
+	d, e := ioutil.ReadAll(reader)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
+}
+
+func Utf8ToGbk(s []byte) ([]byte, error) {
+	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewEncoder())
+	d, e := ioutil.ReadAll(reader)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
+}
+
+func IsUtf8(buf []byte) bool{
+	nBytes := 0
+	for i:= 0;i<len(buf);i++{
+		if nBytes == 0{
+			if (buf[i] & 0x80) != 0 {
+				for (buf[i] & 0x80) != 0 {
+					buf[i] <<= 1
+					nBytes++
+				}
+
+				if nBytes < 2 || nBytes > 6 {
+					return false
+				}
+
+				nBytes--
+			}
+		}else{
+			if buf[i] & 0xc0 != 0x80{
+				return false
+			}
+			nBytes--
+		}
+	}
+	return nBytes == 0
+}
